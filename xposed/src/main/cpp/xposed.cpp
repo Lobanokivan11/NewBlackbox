b20b89feb924e10e895e02996f9fd25b6c3789ae
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-03-13 18:20:21 +0300

    Update xposed.cpp

--- xposed/src/main/cpp/xposed.cpp
+++ xposed/src/main/cpp/xposed.cpp
@@ -49,7 +49,7 @@ JNI_OnLoad(JavaVM* vm, void* reserved) {
         return JNI_ERR;
     }
     LSPosed::ElfImg art(getArtPath().c_str());
-    if (art.getBase() == nullptr) {
+    if (art.base == nullptr) {
             return JNI_ERR;
     }
     lsplant::InitInfo initInfo {
