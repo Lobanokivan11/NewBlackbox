4b918798a70dc5c3cfcd3d4a03ac3018b7f42098
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-04-16 14:45:41 +0300

    Update DirectAccessService.java

--- xposed/src/main/java/de/robv/android/xposed/services/DirectAccessService.java
+++ xposed/src/main/java/de/robv/android/xposed/services/DirectAccessService.java
@@ -7,87 +7,99 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public final class DirectAccessService extends BaseService {
-  public boolean checkFileAccess(String paramString, int paramInt) {
-    File file = new File(paramString);
-    return (paramInt == 0 && !file.exists()) ? false : (((paramInt & 0x4) != 0 && !file.canRead()) ? false : (((paramInt & 0x2) != 0 && !file.canWrite()) ? false : (!((paramInt & 0x1) != 0 && !file.canExecute()))));
-  }
-  
-  public boolean checkFileExists(String paramString) {
-    return (new File(paramString)).exists();
-  }
-  
-  public FileResult getFileInputStream(String paramString, long paramLong1, long paramLong2) throws IOException {
-    File file = new File(paramString);
-    long l1 = file.length();
-    long l2 = file.lastModified();
-    return (paramLong1 == l1 && paramLong2 == l2) ? new FileResult(l1, l2) : new FileResult(new BufferedInputStream(new FileInputStream(paramString), 16384), l1, l2);
-  }
-  
-  public InputStream getFileInputStream(String paramString) throws IOException {
-    return new BufferedInputStream(new FileInputStream(paramString), 16384);
-  }
-  
-  public boolean hasDirectFileAccess() {
-    return true;
-  }
-  
-  public FileResult readFile(String paramString, int paramInt1, int paramInt2, long paramLong1, long paramLong2) throws IOException {
-    byte[] arrayOfByte;
-    File file = new File(paramString);
-    long l1 = file.length();
-    long l2 = file.lastModified();
-    if (paramLong1 == l1 && paramLong2 == l2)
-      return new FileResult(l1, l2); 
-    if (paramInt1 <= 0 && paramInt2 <= 0)
-      return new FileResult(readFile(paramString), l1, l2); 
-    if (paramInt1 <= 0 || paramInt1 < l1) {
-      int i = paramInt1;
-      if (paramInt1 < 0)
-        i = 0; 
-      if (paramInt2 <= 0 || (i + paramInt2) <= l1) {
-        paramInt1 = paramInt2;
-        if (paramInt2 <= 0)
-          paramInt1 = (int)(l1 - i); 
-        arrayOfByte = new byte[paramInt1];
-        FileInputStream fileInputStream = new FileInputStream(file);
-        fileInputStream.skip(i);
-        fileInputStream.read(arrayOfByte);
-        fileInputStream.close();
-        return new FileResult(arrayOfByte, l1, l2);
-      } 
-      StringBuilder stringBuilder1 = new StringBuilder();
-      stringBuilder1.append("Length ");
-      stringBuilder1.append(paramInt2);
-      stringBuilder1.append(" is out of range for ");
-      stringBuilder1.append((String)arrayOfByte);
-      throw new IllegalArgumentException(stringBuilder1.toString());
-    } 
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append("Offset ");
-    stringBuilder.append(paramInt1);
-    stringBuilder.append(" is out of range for ");
-    stringBuilder.append((String)arrayOfByte);
-    throw new IllegalArgumentException(stringBuilder.toString());
-  }
-  
-  public FileResult readFile(String paramString, long paramLong1, long paramLong2) throws IOException {
-    File file = new File(paramString);
-    long l1 = file.length();
-    long l2 = file.lastModified();
-    return (paramLong1 == l1 && paramLong2 == l2) ? new FileResult(l1, l2) : new FileResult(readFile(paramString), l1, l2);
-  }
-  
-  public byte[] readFile(String paramString) throws IOException {
-    File file = new File(paramString);
-    byte[] arrayOfByte = new byte[(int)file.length()];
-    FileInputStream fileInputStream = new FileInputStream(file);
-    fileInputStream.read(arrayOfByte);
-    fileInputStream.close();
-    return arrayOfByte;
-  }
-  
-  public FileResult statFile(String paramString) throws IOException {
-    File file = new File(paramString);
-    return new FileResult(file.length(), file.lastModified());
-  }
+    
+    @Override
+    public boolean checkFileAccess(String paramString, int paramInt) {
+        File file = new File(paramString);
+        if (paramInt == 0) return file.exists();
+        if ((paramInt & 0x4) != 0 && !file.canRead()) return false;
+        if ((paramInt & 0x2) != 0 && !file.canWrite()) return false;
+        if ((paramInt & 0x1) != 0 && !file.canExecute()) return false;
+        return file.exists();
+    }
+
+    @Override
+    public boolean checkFileExists(String paramString) {
+        return (new File(paramString)).exists();
+    }
+
+    @Override
+    public boolean hasDirectFileAccess() {
+        return true;
+    }
+
+    @Override
+    public FileResult getFileInputStream(String paramString, long paramLong1, long paramLong2) throws IOException {
+        File file = new File(paramString);
+        long l1 = file.length();
+        long l2 = file.lastModified();
+        if (paramLong1 == l1 && paramLong2 == l2) {
+            return new FileResult(l1, l2);
+        }
+        return new FileResult(new BufferedInputStream(new FileInputStream(file), 16384), l1, l2);
+    }
+
+    @Override
+    public InputStream getFileInputStream(String paramString) throws IOException {
+        return new BufferedInputStream(new FileInputStream(paramString), 16384);
+    }
+
+    @Override
+    public FileResult statFile(String paramString) throws IOException {
+        File file = new File(paramString);
+        if (!file.exists()) throw new java.io.FileNotFoundException(paramString);
+        return new FileResult(file.length(), file.lastModified());
+    }
+
+    @Override
+    public byte[] readFile(String paramString) throws IOException {
+        File file = new File(paramString);
+        byte[] arrayOfByte = new byte[(int) file.length()];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(arrayOfByte);
+        }
+        return arrayOfByte;
+    }
+
+    @Override
+    public FileResult readFile(String paramString, long paramLong1, long paramLong2) throws IOException {
+        File file = new File(paramString);
+        long l1 = file.length();
+        long l2 = file.lastModified();
+        if (paramLong1 == l1 && paramLong2 == l2) {
+            return new FileResult(l1, l2);
+        }
+        return new FileResult(readFile(paramString), l1, l2);
+    }
+
+    @Override
+    public FileResult readFile(String paramString, int offset, int length, long previousSize, long previousTime) throws IOException {
+        File file = new File(paramString);
+        long currentSize = file.length();
+        long currentTime = file.lastModified();
+
+        if (previousSize == currentSize && previousTime == currentTime) {
+            return new FileResult(currentSize, currentTime);
+        }
+
+        if (offset <= 0 && length <= 0) {
+            return new FileResult(readFile(paramString), currentSize, currentTime);
+        }
+
+        if (offset >= currentSize) {
+            throw new IllegalArgumentException("Offset " + offset + " is out of range for " + paramString);
+        }
+
+        int readLength = length;
+        if (readLength <= 0 || (offset + readLength) > currentSize) {
+            readLength = (int) (currentSize - offset);
+        }
+
+        byte[] arrayOfByte = new byte[readLength];
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            fileInputStream.skip(offset);
+            fileInputStream.read(arrayOfByte);
+        }
+        return new FileResult(arrayOfByte, currentSize, currentTime);
+    }
 }
