065e6b7d8c31c8a60d9f226af9b1d1ca8a5930e7
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-04-16 16:59:58 +0300

    Update ICrossProfileAppsProxy.java

--- Bcore/src/main/java/top/niunaijun/blackbox/fake/service/ICrossProfileAppsProxy.java
+++ Bcore/src/main/java/top/niunaijun/blackbox/fake/service/ICrossProfileAppsProxy.java
@@ -1,24 +1,36 @@
 package top.niunaijun.blackbox.fake.service;
 
+import android.content.Context;
+import android.os.IBinder;
 import java.lang.reflect.Method;
+
 import top.niunaijun.blackbox.fake.hook.BinderInvocationStub;
 import top.niunaijun.blackbox.fake.hook.MethodHook;
 import top.niunaijun.blackbox.fake.hook.ProxyMethod;
-import black.android.os.ServiceManager;
-import android.content.Context;
 
 public class ICrossProfileAppsProxy extends BinderInvocationStub {
 
     public ICrossProfileAppsProxy() {
-        super(ServiceManager.get().getService(Context.CROSS_PROFILE_APPS_SERVICE));
+        super(getSystemBinder());
+    }
+
+    private static IBinder getSystemBinder() {
+        try {
+            Class<?> sm = Class.forName("android.os.ServiceManager");
+            Method getService = sm.getMethod("getService", String.class);
+            return (IBinder) getService.invoke(null, Context.CROSS_PROFILE_APPS_SERVICE);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     protected Object getWho() {
+        IBinder binder = getSystemBinder();
         try {
-            return black.android.content.pm.ICrossProfileApps.Stub.asInterface.call(
-                ServiceManager.get().getService(Context.CROSS_PROFILE_APPS_SERVICE)
-            );
+            Class<?> stubClass = Class.forName("android.content.pm.ICrossProfileApps$Stub");
+            Method asInterfaceMethod = stubClass.getMethod("asInterface", IBinder.class);
+            return asInterfaceMethod.invoke(null, binder);
         } catch (Exception e) {
             return null;
         }
@@ -38,6 +50,7 @@ public class ICrossProfileAppsProxy extends BinderInvocationStub {
     public static class CanInteractAcrossProfiles extends MethodHook {
         @Override
         protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            // Возвращаем false, чтобы избежать SecurityException
             return false;
         }
     }
